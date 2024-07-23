3a45aa0598b62f7b9de1a331673a6de2d536f535
thibaultMA <thibault.m-a@outlook.com>
2024-07-23 16:06:17 +0200

    env

--- backend/controller/stripeController.js
+++ backend/controller/stripeController.js
@@ -1,6 +1,6 @@
 import  Stripe  from 'stripe';
 
-const stripe = Stripe('sk_test_51PXOvV2NiETwuA3LHwdwrf3Bfeml9UH7Kjx6VNKmCMiGpvfzR4RmPQFoRMaxy7ZAWskMd5cCb9Dx83btCtMPd9gV00J22Bjh1U');  // Remplacez par votre clé secrète Stripe
+const stripe = Stripe(process.env.STRIPE_PRIVATE_KEY);  // Remplacez par votre clé secrète Stripe
 
 export async function stripeCekoutSessionCreate(commande) {
     return await stripe.checkout.sessions.create({
